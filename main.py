887cd83e0c949e544e792c30b47483565ee426ae
bhaveshgadag <bhaveshgadag46@gmail.com>
2023-08-07 23:50:51 +0530

    extract from source::yaml file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
+import selectorlib
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
+
 
 def scrapper(url):
     response = requests.get(url)
@@ -9,5 +11,13 @@ def scrapper(url):
     return source
 
 
+def extract(data):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    extracted = extractor.extract(data)["tours"]
+    return extracted
+
+
 if __name__ == "__main__":
-    print(scrapper(URL))
+    source = scrapper(URL)
+    extracted = extract(source)
+    print(extracted)
