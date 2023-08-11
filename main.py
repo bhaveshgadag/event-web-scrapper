0dd9d5a3d00198e64aad3cd9497bef676be9e2e9
bhaveshgadag <bhaveshgadag46@gmail.com>
2023-08-12 01:48:03 +0530

    Wip::  classes

--- main.py
+++ main.py
@@ -4,22 +4,24 @@ import sqlite3
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
 
-connection = sqlite3.connect("data.db")
 
-def store(event):
-    event = event.split(',')
-    cursor = connection.cursor()
-    cursor.execute('INSERT INTO events (band, city, date) VALUES (?,?,?)', event)
-    connection.commit()
+class Database:
+    def __init__(self, database):
+        self.connection = sqlite3.connect(database)
 
+    def store(self, event):
+        event = event.split(',')
+        cursor = self.connection.cursor()
+        cursor.execute('INSERT INTO events (band, city, date) VALUES (?,?,?)', event)
+        self.connection.commit()
 
-def read(event):
-    event = event.split(',')
-    cursor = connection.cursor()
-    cursor.execute('SELECT band, city, date FROM events WHERE band=? ' \
-            'AND city=? AND date=?', event)
-    result = cursor.fetchall()
-    return result
+    def read(self, event):
+        event = event.split(',')
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT band, city, date FROM events WHERE band=? AND city=? AND date=?', event)
+        result = cursor.fetchall()
+        return result
+
 
 if __name__ == "__main__":
     while True:
@@ -27,8 +29,7 @@ if __name__ == "__main__":
         extracted = sf.extract(source)
 
         if extracted != "No upcoming tours":
-            if not read(extracted):
-                store(extracted)
-                send_email(extracted)
-
-    connection.close()
+            db = Database(database="data.db")
+            if not db.read(extracted):
+                db.store(extracted)
+                # send_email(extracted)
